925e7d5328a7f794a38a49ea2e7d08a2fc948bf8
Ezra <12880287+ezraaaa@users.noreply.github.com>
2023-06-02 22:46:07 +0800

    Initial commit

---
+++ lib/exceptions_flutter.dart
@@ -0,0 +1,4 @@
+/// Repository which provides exceptions in a format to users
+library exceptions_flutter;
+
+export 'src/exceptions_flutter.dart';
---
+++ test/src/exceptions_flutter_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:exceptions_flutter/exceptions_flutter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('ExceptionsFlutter', () {
+    test('can be instantiated', () {
+      expect(ExceptionsFlutter(), isNotNull);
+    });
+  });
+}
